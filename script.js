03ca01b5cf88394376d79a4392d3ab000566bdca
DJStompZone <85457381+DJStompZone@users.noreply.github.com>
2023-11-24 11:26:48 +0000

    Update url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 setTimeout(function() {
-  let fallbackUrl = "https://discord.com/invite/juTknfc7vr"
+  let fallbackUrl = "https://discord.gg/wYQwBwkBGg"
   window.location.href = process.env.DISCORD_INVITE_LINK ?? fallbackUrl;
 }, 5000);
